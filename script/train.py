000eff3f2eb0652a3ee8e0c684b9280329f469b2
rimeless <48581374+rimeless@users.noreply.github.com>
2024-11-22 22:20:28 +0900

    update

--- script/train.py
+++ script/train.py
@@ -15,31 +15,29 @@ if __name__ == '__main__':
                         default='result')
     args = parser.parse_args()
 
-# cmp_apm = pd.read_csv('/spstorage/USERS/gina/test/data/apm_compound.csv',index_col=0)
-# pck_apm = pd.read_csv('/spstorage/USERS/gina/test/data/apm_protein.csv',index_col=0)
-# dtis = pd.read_csv('/spstorage/USERS/gina/test/data/interaction.csv', header=None)
-
-
+# Load datasets
 dtis = pd.read_csv(args.label_file, header=None)
 cmp_apm = pd.read_csv(args.cmp_apm_file, index_col =0)
 pck_apm = pd.read_csv(args.pck_apm_file, index_col =0)
 
+# Prepare training, validation, and test datasets
 X_train,y_train = setting_dataset(cmp_apm, pck_apm, dtis, 'train',30, 55)
 X_dev,y_dev = setting_dataset(cmp_apm, pck_apm, dtis, 'dev',30, 55)
 X_test,y_test = setting_dataset(cmp_apm, pck_apm, dtis, 'test',30, 55)
 
-
+# Initialize the CNN model
 torch.manual_seed(7)
-model = CNN_model(55, 7, 0.3, 30)
+model = CNN_model(55, 7, 0.3, 30) # Model with specific parameters (input size, filters, dropout, kernel size)
 model.to(device)
 
+# Define optimizer, loss function, and learning rate scheduler
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = torch.nn.BCELoss()
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.9)
 model.zero_grad()
 
 
-
+# Training loop with early stopping
 with open(f'{args.log_dir}/log', 'a') as f:
     best_model_wts = copy.deepcopy(model.state_dict())
     best_val_rocauc = 0.5
@@ -48,6 +46,7 @@ with open(f'{args.log_dir}/log', 'a') as f:
     for epoch in range(20):
         losses = []
         accs = []
+        # Mini-batch training
         with tqdm(range(0, len(X_train), 80)) as tepoch:
             for i in tepoch:
                 tepoch.set_description(f"Epoch {epoch + 1}")
@@ -57,9 +56,12 @@ with open(f'{args.log_dir}/log', 'a') as f:
                 except:
                     gc.collect()
                     continue
+                # Forward pass and compute loss
                 acc, loss, _ = fwd_pass(batch_X, batch_y, model, optimizer, criterion, train=True)
                 losses.append(loss.item())
                 accs.append(acc)
+
+                # Update progress bar with metrics
                 acc_mean = np.array(accs).mean()
                 loss_mean = np.array(losses).mean()
                 tepoch.set_postfix(loss=loss_mean, accuracy=100. * acc_mean)
@@ -69,6 +71,7 @@ with open(f'{args.log_dir}/log', 'a') as f:
         test_rocauc = test_func(model, y_test, X_test)
         f.write(
             f"{epoch},{round(float(acc), 5)},{round(float(val_rocauc), 5)},{round(float(test_rocauc), 5)}\n")       
+        # Early stopping logic
         if early >= epochs_early_stop:
             model.load_state_dict(best_model_wts)
             torch.save(model.state_dict(), f"{arg.checkpoint_dir}/train_model.pth")
@@ -77,6 +80,7 @@ with open(f'{args.log_dir}/log', 'a') as f:
             best_val_rocauc = float(val_rocauc)
             best_model_wts = copy.deepcopy(model.state_dict())
             early=0
+        # Save model weights after each epoch
         model.load_state_dict(best_model_wts)
         torch.save(model.state_dict(), f"{arg.checkpoint_dir}/train_model.pth")
 
