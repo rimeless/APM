1826c2a7361b012df5a738309fc3cf734688bb84
rimeless <48581374+rimeless@users.noreply.github.com>
2024-11-22 22:20:09 +0900

    update

--- script/main.py
+++ script/main.py
@@ -14,19 +14,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-
-model = CNN_model(55, 7, 0.3, 30)
+# Initialize the convolutional neural network (CNN) model
+model = CNN_model(55, 7, 0.3, 30)  # Model with input size 55, 7 filters, dropout rate 0.3, and kernel size 30
 model.to(device)
-model.load_state_dict(torch.load(args.checkpoint_file))
+model.load_state_dict(torch.load(args.checkpoint_file)) # Load pre-trained weights into the model
 
+# Load compound and protein APM 
 cmp_apm = pd.read_csv(args.cmp_apm_file, index_col =0)
 pck_apm = pd.read_csv(args.pck_apm_file, index_col =0)
 
+# Convert compound APM data into a 4D tensor (for CNN input)
 apm = np.array(cmp_apm) 
 apm = torch.tensor(apm, dtype = torch.float).view(len(apm),1,55,10)
-ppock = torch.tensor(np.array(pck_apm, dtype = float).reshape([-1,55,10]), dtype = torch.float)
-X = [[apm[i], ppock] for i in range(len(apm))]
 
+# Convert protein pocket APM data into a tensor
+pocket = torch.tensor(np.array(pck_apm, dtype = float).reshape([-1,55,10]), dtype = torch.float)
+X = [[apm[i], pocket] for i in range(len(apm))]
+
+# Make predictions using the trained model
 probs = []
 for idx, X_test in enumerate(X):
     x = [0, 0]
@@ -36,8 +41,7 @@ for idx, X_test in enumerate(X):
     prob = res[0].item()
     probs.append([idx, prob])
 
-
-
+# Create a DataFrame to store prediction results
 dti_table = pd.DataFrame(probs)
 dti_table.columns = ['compound','probability']
 dti_table.compound = list(cmp_apm.index)
